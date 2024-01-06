6fdaa6c3df421a8a4c43c01920eeffd691f94ac1
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-01-06 09:03:56 +0800

    New updates

---
+++ src/main/java/com/example/weatherapplication/RunWeather.java
@@ -0,0 +1,11 @@
+package com.example.weatherapplication;
+
+import javafx.application.Application;
+
+public class RunWeather {
+    public static void main(String[] args) {
+        // Launch the application
+        Application.launch(Weather_Application.class, args);
+
+    }
+}
